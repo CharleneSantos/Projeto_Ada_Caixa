105ac98076f607fef26c3f8299952bae8509dbd8
Charlene <charlenesantos8472@gmail.com>
2025-10-01 20:02:27 -0300

    Criação das medidas e relatorio CLI

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-
+#%%
 import kagglehub
 import os
 import csv
 import shortuuid
 import matplotlib.pyplot as plt
-from collections import Counter
 import streamlit as st
-from funcoes import valores_unicos, adicionar_novo_item, medias, medias_por_area_conhecimento
+from funcoes import adicionar_novo_item, medias, mostrar_medias_por_area, valores_unicos, calcular_nota_media, valores_unicos_por_grupo, porcentagem_por_grupos, calcular_estatisticas, st_porcentagem_por_grupos, salvar_dic_em_csv
 
 
 path = kagglehub.dataset_download("spscientist/students-performance-in-exams")
@@ -30,67 +29,131 @@ for i, item in enumerate(dados):
         dados[i] = novo_item
 
 
-v_unicos = valores_unicos(dados)
-for k, v in v_unicos.items():
-    valores = list(v)
-    print(f"{k}: {len(v)} valores únicos ---> {valores}")
-
-
-#%%Criar função para calcular médias/min/máx/mediana/desvio padrão/%quartis
-nota_leitura_genero = medias(dados, 'gender', 'reading score' )
-medias_matematica_por_grupo = medias_por_area_conhecimento(dados, 'math score' )
-nota_media_matematica = sum([float(item['math score']) for item in dados]) / len(dados)
-medias_leitura_por_grupo = medias_por_area_conhecimento(dados, 'reading score' )
-nota_media_leitura = sum([float(item['reading score']) for item in dados]) / len(dados)
-medias_escrita_por_grupo = medias_por_area_conhecimento(dados, 'writing score' )
-nota_media_escrita = sum([float(item['writing score']) for item in dados]) / len(dados)
-
-print(f"Medias das notas de leitura por grupo:\n {medias_leitura_por_grupo}")
-print(f"Nota media de matematica: {nota_media_matematica}")
-# %%
+salvar_dic_em_csv("StudentsPerformance_salvo.csv", dados)
 
 
 
-def porcentagem_generos(dados, campo='gender'):
-    contagem = Counter()
-    total = len(dados)
-
-    for linha in dados:
-        genero = linha.get(campo, '').strip().lower()
-        if genero in ['male', 'female']:
-            contagem[genero] += 1
-
-    porcentagens = {g: round((contagem[g] / total) * 100, 2) for g in contagem}
-    return porcentagens
-
-pc_generos = porcentagem_generos(dados=dados, campo='gender').values()
+################Relatórios e Análises#####################
 
 
-#%%
-plt.figure(figsize=(6, 6))
-plt.pie(pc_generos, labels=['Masculino', 'Feminino'], autopct='%1.1f%%', colors=['blue', 'salmon'])
-plt.title('Estudantes Avaliados(por gênero)')
-plt.show()
-# %%
+print(f'''
+      ----------------DADOS GERAIS----------------
+      
+    
+Numero total de registros: {len(dados)}
 
-st.title('Análise de Gênero')
 
-# Chama a função para obter as porcentagens
+% TOTAL DE ESTUDANTES POR GRUPO:
 
-pc_generos_st = porcentagem_generos(dados=dados, campo='gender')
+{porcentagem_por_grupos(dados, 'gender' )}
+{porcentagem_por_grupos(dados, 'race/ethnicity' )}
+{porcentagem_por_grupos(dados, 'parental level of education' )}
+{porcentagem_por_grupos(dados, 'lunch' )}
+{porcentagem_por_grupos(dados, 'test preparation course' )}
+      ''')
 
-if pc_generos_st:
-    # Separa os rótulos e valores para o gráfico de pizza
-    rotulos = pc_generos_st.keys()
-    valores = pc_generos_st.values()
+
+valores_unicos(dados, 'gender','race/ethnicity', 'parental level of education','lunch', 'test preparation course')
+
+
+print(f'''
+ESTATÍSTICAS DESCRTIVAS - Matemática:''')
+est_matematica = calcular_estatisticas('math score', dados)
+
+for k, v in est_matematica.items():
+     print(f'\t{k}: {v}')
+
+print(f'''
+      ESTATÍSTICAS DESCRTIVAS - Leitura:''')
+est_leitura = calcular_estatisticas('reading score', dados)
+for k, v in est_leitura.items():
+     print(f'\t{k}: {v}')
+
+print(f'''
+      ESTATÍSTICAS DESCRTIVAS - Escrita:''')
+est_escrita = calcular_estatisticas('writing score', dados)
+for k, v in est_escrita.items():
+	 print(f'\t{k}: {v}')
+
+
+nota_media_matematica = calcular_nota_media('math score', dados)
+nota_media_leitura = calcular_nota_media('reading score', dados)
+nota_media_escrita = calcular_nota_media('writing score', dados)
+
+print(f'''
+NOTAS MÉDIAS POR ÁREA DO CONHECIMENTO:
+      
+	matemática: {nota_media_matematica}
+    
+	leitura: {nota_media_leitura}
+    
+	escrita:{nota_media_escrita}''')
+
+
+
+print(f'''
+NOTAS MÉDIAS POR GRUPOS: 
+      ''')
+      
+mostrar_medias_por_area(dados, ['math score', 'reading score', 'writing score'])
+
+
+
+
+
+# #%%
+
+azul_caixa = '#005CA9'
+laranja_caixa = '#F39200'		
+tangerina = '#F9B000'
+ceu = '#00B5E5'  # Azul Céu
+goiaba = '#EF765E'
+turquesa = '#54BBAB'
+uva = '#B26F9B'
+limao = '#AFCA0B'  # Limão
+azul_marinho = '#004198'
+branca = '#FFFFFF'
+
+
+
+
+
+st.write(f"<h1 style='color: {azul_marinho};text-align: center'>Análise da Performance de Estudantes em Testes</h1>", unsafe_allow_html=True)
+st.sidebar.title("Filtros")
+col1, col2 = st.columns(2)
+
+pc_genero_st = st_porcentagem_por_grupos(dados, 'gender')
+if pc_genero_st:
+    
+    rotulos = list(pc_genero_st.keys())
+    valores = list(pc_genero_st.values())
     # Cria a figura e os eixos do Matplotlib
-    fig, ax = plt.subplots(figsize=(6, 6))
-    ax.pie(valores, labels=rotulos, autopct='%1.1f%%', colors=['lightgreen', 'salmon'])
-    ax.set_title('Proporção por Gênero')
+    fig, ax = plt.subplots(figsize=(6, 6))  # Tamanho ajustado para o Streamlit
+    ax.pie(valores, 
+           	labels=rotulos, 
+        	autopct='%1.1f%%', 
+            colors=['lightgreen', 'salmon'],
+            textprops={'fontsize':8},
+            
+    )
+    ax.set_title('Proporção por Gênero', fontsize=12, loc='left')
+    col1.pyplot(fig)
 
-    # Exibe o gráfico no Streamlit usando st.pyplot()
-    st.pyplot(fig)
+
 else:
     st.warning('Não há dados de gênero válidos para exibir.')
+    
+
+
+
+
+# #%%
+
+
+
+# print(est_matematica['minimo'])
+# #%% 
+# # Criar função para calcular correlação entre variavel nuimerica e categorica
+
 
 # %%
--- funcoes.py
+++ funcoes.py
@@ -2,6 +2,8 @@
 import shortuuid
 from itertools import groupby
 from operator import itemgetter
+from collections import Counter
+
 
 def adicionar_novo_item(item_novo, dados):
     '''
@@ -11,14 +13,41 @@ def adicionar_novo_item(item_novo, dados):
     dados.append(item_novo_com_id)
 
 
-def valores_unicos(linhas):
+
+def valores_unicos_por_grupo(linhas, grupo):
     unicos = {}
     for linha in linhas:
         for k, v in linha.items():
             unicos.setdefault(k, set()).add(v)
         
-    return unicos
+    return unicos[grupo]
 
+
+def valores_unicos(dados, *grupos):
+    print("VALORES ÚNICOS POR GRUPO")
+    for grupo in grupos:
+        valores = valores_unicos_por_grupo(dados, grupo)
+        print(f"\t{grupo}: {len(valores)} valores únicos ---> {list(valores)}")
+
+def porcentagem_por_grupos(dados, grupo):
+    contagem = Counter()
+    total = len(dados)
+    valores = list(valores_unicos_por_grupo(dados, grupo))
+
+
+    for linha in dados:
+        valor = linha.get(grupo, '')
+        if valor in valores:
+            contagem[valor] += 1
+
+    porcentagens = {g: round((contagem[g] / total) * 100, 2) for g in contagem}
+    retorno  = f'''{grupo}:\n'''
+    
+    for g, p in sorted(porcentagens.items()):
+        retorno += f'''     {g}: {p}%\n'''
+
+    return retorno
+    
 
 def medias(dados, campo_grupo, coluna):
     
@@ -27,20 +56,80 @@ def medias(dados, campo_grupo, coluna):
     medias = {}
     for grupo, itens in groupby(dados_ordenados, key=itemgetter(f'{campo_grupo}')):
         scores = [float(item[coluna]) for item in itens]
-        medias[grupo] = sum(scores) / len(scores)
+        medias[grupo] = round(sum(scores) / len(scores),2)
 
     return medias
 
-def medias_por_area_conhecimento(dados, area_conhecimento):
-    nome_colunas = (list(dados[0].keys()))
-    grupos = nome_colunas[1:6]
-    # print(f"{area_conhecimento.upper()}")
+def mostrar_medias_por_area(dados, areas_conhecimento):
+    nome_colunas = list(dados[0].keys())
+    grupos = nome_colunas[1:6]  # Ajuste conforme os grupos desejados
 
-    infos = []
-    for grupo in grupos: 
-        medias_g = medias(dados, grupo, area_conhecimento)
-        infos.append((grupo, medias_g))
-    return infos 
+
+    for area in areas_conhecimento:
+        print(f"\nMedias {area.upper()} por grupo")
+        for grupo in grupos:
+            medias_g = medias(dados, grupo, area)
+            print(f"\n\tGrupo: {grupo}")
+            for categoria, valor in sorted(medias_g.items(), key=lambda x: x[1], reverse=True):
+                print(f"\t- {categoria}: {round(valor, 2)}")
+
+
+            
+
+
+def calcular_nota_media(area_conhecimento, dados):
+
+    nota = round(sum([float(item[area_conhecimento]) for item in dados]) / len(dados),2)
+  
+    return nota
+
+
+
+
+def calcular_estatisticas(area_conhecimento, dados):
+    from statistics import mean, median, stdev, quantiles
+
+    notas = [float(item[area_conhecimento]) for item in dados]
+    estatisticas = {
+        'mínimo': round(min(notas), 2),
+        'máximo': round(max(notas), 2),
+        'média': round(mean(notas), 2),
+        'mediana': round(median(notas), 2),
+        'desvio padrão': round(stdev(notas), 2),
+        'quartis': [round(q, 2) for q in quantiles(notas, n=4)]
+    }
+    return estatisticas
+
+def st_porcentagem_por_grupos(dados, grupo):
+    from collections import Counter
+
+    contagem = Counter()
+    total = len(dados)
+    valores = list(valores_unicos_por_grupo(dados, grupo))
+
+    for linha in dados:
+        valor = linha.get(grupo, '').strip()
+        if valor in valores:
+            contagem[valor] += 1
+
+    porcentagens = {g: round((contagem[g] / total) * 100, 2) for g in contagem}
+    return porcentagens
+
+
+
+
+def salvar_dic_em_csv(arquivo_novo, dataset):
+# novo_arquivo = "StudentsPerformance_salvo.csv"
+    import csv
+
+    with open(arquivo_novo, mode='w', newline='', encoding='utf-8') as f_out:
+        campos = dataset[0].keys()  # Usa os cabeçalhos do primeiro dicionário
+        escritor = csv.DictWriter(f_out, fieldnames=campos)
+        escritor.writeheader()
+        escritor.writerows(dataset)
+
+        print(f"✅ Arquivo salvo como '{arquivo_novo}' com sucesso!")
+
 
 
 
